96870d3bb11a89d7c67e51ffcb7316bb1c795225
theobourgeois <theoobourgeois@gmail.com>
2024-03-06 22:45:46 -0400

    linintg fix

--- src/components/graph/edges.tsx
+++ src/components/graph/edges.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// TODO remove this ^ when you're using control points
 import { useTheme } from "../../utils/theme-utils";
 import { Dimensions } from "../../utils/typings-utils";
 import { snapTo } from "../../utils/utils-fns";
